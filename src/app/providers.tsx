7255a66dadfea58b300432ec75d519a7e691ee44
Ghassen MENAOUAR <ghassen.menaouar@gmail.com>
2025-09-08 21:57:36 +0100

    feat: Introduce Providers component to encapsulate QueryClientProvider and AuthProvider, streamlining the layout structure for better data management and authentication handling.

---
+++ src/app/providers.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "@/hooks/use-auth";
+import { getQueryFn } from "@/lib/queryClient";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            queryFn: getQueryFn({ on401: "throw" }),
+            refetchInterval: false,
+            refetchOnWindowFocus: false,
+            staleTime: Infinity,
+            retry: false,
+          },
+          mutations: {
+            retry: false,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>{children}</AuthProvider>
+    </QueryClientProvider>
+  );
+}
